928695cd60e8903d65d129196b6a31f13811ef28
Rabbit <dre0475@gmail.com>
2021-01-18 23:45:37 +0800

    更新1

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("00000000000");
+        System.out.println("我是dev分支");
     }
 }
